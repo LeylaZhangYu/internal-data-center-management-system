3971c793f076dd848ef660d706ede950bb491a58
LeylaZhangYu <zyh001109@gmail.com>
2026-09-08 09:25:16 +0800

    resolve Could not validate credentials

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -6,10 +6,20 @@ const client = axios.create({
 
 client.interceptors.request.use((config) => {
   const token = localStorage.getItem('dc_token')
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`
-  }
+  if (token) config.headers.Authorization = `Bearer ${token}`
   return config
 })
+
+client.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error?.response?.status === 401) {
+      localStorage.removeItem('dc_token')
+      localStorage.removeItem('dc_user')
+      if (window.location.pathname !== '/login') window.location.assign('/login')
+    }
+    return Promise.reject(error)
+  },
+)
 
 export default client
